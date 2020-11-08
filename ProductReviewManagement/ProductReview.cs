a7fbe49d61051be5904c83178c9f80b26bc357ab
kretika arora <kretikaarora@gmail.com>
2020-11-08 12:01:28 +0530

    [Kretika Arora]Welcome to Product Review System

---
+++ ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProductReview.cs" company="Capgemini">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Kretika Arora"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    /// <summary>
+    /// Product Review Class
+    /// </summary>
+     public class ProductReview
+     {
+        public int productId { get; set; }
+        public int userId { get; set; }
+        public double rating { get; set; }
+        public string review { get; set; }
+        public bool isLike { get; set; }
+     }
+}
---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Capgemini">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Kretika Arora"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    /// <summary>
+    /// Program Class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Creating a List of ProductReview and Adding values into List
+        /// UC1 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Product Review Management System");
+
+          
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+               new ProductReview(){productId=1,userId=1,rating=1,review="Low",isLike=true},
+               new ProductReview(){productId=2,userId=2,rating=2,review="Low",isLike=true},
+               new ProductReview(){productId=3,userId=3,rating=3,review="Average",isLike=true},
+               new ProductReview(){productId=4,userId=4,rating=4,review="Average",isLike=true},
+               new ProductReview(){productId=5,userId=5,rating=5,review="Good",isLike=true},
+               new ProductReview(){productId=6,userId=6,rating=6,review="Good",isLike=true},
+               new ProductReview(){productId=7,userId=7,rating=1,review="Low",isLike=true},
+               new ProductReview(){productId=8,userId=8,rating=2,review="Low",isLike=true},
+               new ProductReview(){productId=9,userId=9,rating=3,review="Average",isLike=true},
+               new ProductReview(){productId=10,userId=10,rating=4,review="Good",isLike=true},
+               new ProductReview(){productId=11,userId=11,rating=5,review="Good",isLike=true},
+            };
+
+            foreach(var productReview in productReviewList)
+            {
+                Console.WriteLine($"ProductId : {productReview.productId}, UserId : {productReview.userId}, Rating : {productReview.rating}, Review : {productReview.review}, isLike :{productReview.isLike}");
+            }
+        }
+    }
+}
